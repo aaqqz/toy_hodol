ef7b66d72c47840642f8ab0b08cea9b1734a2100
hg <sswwx1@naver.com>
2025-07-12 11:35:37 +0900

    first commit

---
+++ src/main/java/com/hodol/toy_hodol/ToyHodolApplication.java
@@ -0,0 +1,13 @@
+package com.hodol.toy_hodol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyHodolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyHodolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hodol/toy_hodol/ToyHodolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hodol.toy_hodol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyHodolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
