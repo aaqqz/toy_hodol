50956b26bd5aae67da5be32dcb72184a13151518
hg <sswwx1@naver.com>
2025-08-07 23:54:19 +0900

    cookie 인증 방식

--- src/main/java/com/hodol/toy_hodol/common/resolver/AuthResolver.java
+++ src/main/java/com/hodol/toy_hodol/common/resolver/AuthResolver.java
@@ -5,7 +5,10 @@ import com.hodol.toy_hodol.common.exception.ErrorCode;
 import com.hodol.toy_hodol.common.resolver.data.UserSession;
 import com.hodol.toy_hodol.domain.auth.entity.Session;
 import com.hodol.toy_hodol.domain.auth.repository.SessionRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -13,6 +16,7 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
@@ -26,14 +30,26 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        String accessToken = webRequest.getHeader("Authorization");
-        if ( accessToken == null || accessToken.isEmpty() ) {
+//        String accessToken = webRequest.getHeader("Authorization");
+//        if ( accessToken == null || accessToken.isEmpty() ) {
+//            throw new CustomException(ErrorCode.UNAUTHORIZED);
+//        }
+
+        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+        if (servletRequest == null){
+            log.error("HttpServletRequest is null");
             throw new CustomException(ErrorCode.UNAUTHORIZED);
         }
 
+        Cookie[] cookies = servletRequest.getCookies();
+        if (cookies.length == 0) {
+            log.error("No cookies found in request");
+            throw new CustomException(ErrorCode.UNAUTHORIZED);
+        }
+        String accessToken = cookies[0].getValue();
+
         Session session = sessionRepository.findByAccessToken(accessToken)
                 .orElseThrow(() -> new CustomException(ErrorCode.UNAUTHORIZED));
-
 
         return UserSession.builder()
                 .id(session.getUser().getId())
--- src/main/java/com/hodol/toy_hodol/domain/auth/controller/AuthController.java
+++ src/main/java/com/hodol/toy_hodol/domain/auth/controller/AuthController.java
@@ -7,10 +7,15 @@ import com.hodol.toy_hodol.domain.auth.service.response.SessionResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @Slf4j
 @RestController
@@ -21,7 +26,20 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ApiResponse<SessionResponse> signin(@RequestBody @Valid AuthLoginRequest request) {
-        return ApiResponse.success(authService.signin(request));
+    public ResponseEntity<Object> signin(@RequestBody @Valid AuthLoginRequest request) {
+        SessionResponse session = authService.signin(request);
+        ResponseCookie cookie = ResponseCookie.from("accessToken", session.getAccessToken())
+                .domain("localhost") // todo 서버 환경에 따른 분리 필요
+                .path("/")
+                .httpOnly(true)
+                .secure(false)
+                .maxAge(Duration.ofDays(30))
+                .sameSite("Strict")
+                .build();
+
+        log.info("########## {}", cookie.toString());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
     }
 }
